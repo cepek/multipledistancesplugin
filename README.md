518210a2dbcec05d7f38ff9dc5a6819227e0d568
Celtian <dominik.hladik@seznam.cz>
2016-05-10 23:36:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# multipledistancesplugin
+
+MultipleDistancesPlugin class is a plugin for gama-q2 adjustment program from Qt based GUI of project GNU gama https://www.gnu.org/software/gama/.  To build the plugin you need Qtcreator or Qt platform with Cmake installed and source codes from GNU gama project, the plugin is build as a Qt subproject. 
+
+* download GNU gama sources from git
+
+        git clone git://git.sv.gnu.org/gama/qt.git
+        cd  qt/gama-q2/lib/
+        git clone git://git.sv.gnu.org/gama.git
+
+* go to qt/gama-q2-plugins
+
+        cd  ../../gama-q2-plugins
+
+* clone multipledistancesplugin and add it as a subproject to gama-q2.pro
+
+        git clone https://github.com/Celtian/multipledistancesplugin/
+        # add line "SUBDIRS += gama-q2-plugins/multipledistancesplugin" to qt/gama-qt.pro
+        
+* build project qt/gama-qt.pro
+
+Enjoy!
---
+++ multipledistancesplugin.h
@@ -0,0 +1,114 @@
+/*  Extension of Gama-q2 for excluding outlying distances
+    based on Grubb's test.
+    Copyright (C) 2016 Dominik Hladík
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+#ifndef MULTIPLEDISTANCESPLUGIN_H
+#define MULTIPLEDISTANCESPLUGIN_H
+
+#include <QObject>
+#include <QString>
+#include <QTableWidget>
+#include <QMainWindow>
+#include <QPushButton>
+#include <QDialog>
+#include <QDialogButtonBox>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QWidget>
+#include <QScrollArea>
+#include <QGroupBox>
+#include <QMessageBox>
+#include <QCheckBox>
+#include <QList>
+#include <QStatusBar>
+#include <QAction>
+#include <QMenu>
+#include <QMenuBar>
+#include <QSpinBox>
+
+#include "gamaq2interfaces.h"
+#include "data.h"
+#include "datalist.h"
+#include "mytablewidgetitem.h"
+#include "help.h"
+
+namespace GNU_gama {
+namespace local {
+    class LocalNetwork;
+}
+}
+
+using LocalNetwork = GNU_gama::local::LocalNetwork;
+
+class QFrame;
+
+class MultipleDistancesPlugin : public QObject, AdjustmentInterface
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.gnu.gama.gama-q2.AdjustmentInterface/1.0")
+    Q_INTERFACES(AdjustmentInterface)
+
+public:
+    MultipleDistancesPlugin();
+
+    QString getName();
+    QWidget* create(QString cname, LocalNetwork* ln);
+
+private:
+
+    QString title = "Multiple Distances plugin";
+
+    enum columns{Id,Sort,Type,PointA,PointB,Count,CountAB,CountBA,Average,Testing};
+
+    QAction *actionClose,*actionType, *actionCount, *actionTest, *actionSettings, *actionReset,
+            *actionDistance, *actionSlopeDistance, *actionUncontrolledDistance,
+            *actionAbout, *actionHelp;
+
+    QColor color1 = QColor(255,255,255);
+    QColor color2 = QColor(240,240,240);
+
+    int statusBarTime = 4000;
+    int precission = 4;
+
+    QString confName;
+    LocalNetwork* lnet {nullptr};
+
+    DataList *dataList;
+    QMainWindow* mainWindow;
+    QTableWidget* tableWidget;
+
+    QMainWindow*  getMainWindow();
+    QDialog* getSettings();
+
+    void createMenuBar();
+    QTableWidget* getTable();
+    QTableWidget* getTest(const int &row, QList<Data*> dataList);
+    double calculatePercentage(const int &row, QList<Data*> dataList);
+    void filter();
+    void filter(const int &row);    
+    void reset();
+    void setTableData();
+
+    static double round(const double &value, const int &decimals);
+
+private slots:
+    void about();
+
+};
+
+#endif // MULTIPLEDISTANCESPLUGIN_H
